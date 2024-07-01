280490bdea11398b07b133ffdcab6738fc7e83bc
IrinaBelonogova <irina.triniti@gmail.com>
2024-07-01 23:34:15 +0300

    calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,37 +1,6 @@
 import game from '../index.js';
 import getRandomNumber from '../random.js';
 
-/*const description = 'What is the result of the expression?';
-  const calculateExpression = (expression) => {
-    const [num1, operation, num2] = expression.split(' ');
-    const num1Value = Number(num1);
-    const num2Value = Number(num2);
-    switch (operation) {
-      case '+':
-        return num1Value + num2Value;
-      case '-':
-        return num1Value - num2Value;
-        case '*':
-        return num1Value * num2Value;
-      default:
-        throw new Error(`Unknown operation: ${operation}`);
-    }
-  }
-
-function generateExpression() {
-    const operations = ['+', '-', '*'];
-    const operationIndex = Math.floor(Math.random() * operations.length);
-    const operation = operations[operationIndex];
-    const num1 = Math.floor(Math.random() * 100);
-    const num2 = Math.floor(Math.random() * 100);
-    return `${num1} ${operation} ${num2}`;
-  }
-  
-  const getQuestionAndAnswer = () => {
-    const question = getRandomNumber();
-    const correctAnswer = calculateExpression(question).toString(); 
-    return [question, correctAnswer];
-  }*/
 const operationSymbols = ['+', '-', '*'];
 const description = 'What is the result of the expression?';
     
